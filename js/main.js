d9d7738b785e18ea25c6c62083a064609fd239ad
Arsen Asanov <tatarin08082002@gmail.com>
2023-10-31 13:14:55 +0300

    latest styles added

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ $(function () {
     arrows: false,
   })
 
+  $('.menu__btn, .menu__list a').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.header__top').toggleClass('header__top--active');
+  });
+
+  $(".menu a, .logo").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'), top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
 });
